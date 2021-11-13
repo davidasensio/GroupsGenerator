b7ba1b725edb1daba1128c5872c0edb612ace23b
David Asensio <david.asensio@xing.com>
2021-11-13 12:52:27 +0100

    Basic Master/Detail

---
+++ app/src/main/java/com/handysparksoft/groupsgenerator/ui/screens/detail/DetailScreen.kt
@@ -0,0 +1,28 @@
+package com.handysparksoft.groupsgenerator.ui.screens.detail
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.handysparksoft.groupsgenerator.ui.GroupsGeneratorApp
+
+@Composable
+fun DetailScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Detail Screen")
+    }
+}
+
+@Preview
+@Composable
+fun DetailScreenPreview() {
+    GroupsGeneratorApp {
+        DetailScreen()
+    }
+}
--- app/src/main/java/com/handysparksoft/groupsgenerator/ui/screens/main/MainScreen.kt
+++ app/src/main/java/com/handysparksoft/groupsgenerator/ui/screens/main/MainScreen.kt
@@ -1,16 +1,41 @@
 package com.handysparksoft.groupsgenerator.ui.screens.main
 
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.handysparksoft.groupsgenerator.model.getGroups
+import com.handysparksoft.groupsgenerator.ui.GroupsGeneratorApp
 
 @Composable
 fun MainScreen() {
     Scaffold(
         topBar = { MainAppBar() }
     ) {
-        Text(text = "Content", modifier = Modifier.padding(it))
+        LazyColumn(
+            modifier = Modifier.fillMaxSize(),
+            contentPadding = PaddingValues(4.dp),
+            verticalArrangement = Arrangement.spacedBy(4.dp)
+        ) {
+            items(getGroups()) { group ->
+                GroupItem(group)
+            }
+        }
     }
 }
+
+@Preview
+@Composable
+fun MainScreenPreview() {
+    GroupsGeneratorApp {
+        MainScreen()
+    }
+}
+
+
